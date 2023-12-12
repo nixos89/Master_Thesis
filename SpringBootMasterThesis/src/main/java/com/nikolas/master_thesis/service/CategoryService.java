389f44e12cae304e0b73c07010d0e779da821e03
Nikola Stevanović <nixos89@gmail.com>
2023-12-12 12:37:18 +0100

    Add empty spacing between enclosing curly braces

--- SpringBootMasterThesis/src/main/java/com/nikolas/master_thesis/service/CategoryService.java
+++ SpringBootMasterThesis/src/main/java/com/nikolas/master_thesis/service/CategoryService.java
@@ -5,9 +5,11 @@ import com.nikolas.master_thesis.dto.CategoryDTO;
 import com.nikolas.master_thesis.dto.CategoryListDTO;
 
 public interface CategoryService {
+
 	CategoryDTO getCategory(Long id);
 	CategoryListDTO findAllCategories();
 	CategoryDTO addCategory(AddCategoryDTO addCategoryDTO);
 	Boolean updateCategory(AddCategoryDTO addCategoryDTO, Long id);
 	Boolean deleteCategory(Long id);
+
 }
