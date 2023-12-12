993e97ad5245422e3dc815bc717f03b1ef02808a
Nikola Stevanović <nixos89@gmail.com>
2023-12-12 12:36:33 +0100

    Remove empty spacing between lines

--- SpringBootMasterThesis/src/main/java/com/nikolas/master_thesis/service/CategoryService.java
+++ SpringBootMasterThesis/src/main/java/com/nikolas/master_thesis/service/CategoryService.java
@@ -5,14 +5,9 @@ import com.nikolas.master_thesis.dto.CategoryDTO;
 import com.nikolas.master_thesis.dto.CategoryListDTO;
 
 public interface CategoryService {
-	
 	CategoryDTO getCategory(Long id);
 	CategoryListDTO findAllCategories();
-
-
 	CategoryDTO addCategory(AddCategoryDTO addCategoryDTO);
-
 	Boolean updateCategory(AddCategoryDTO addCategoryDTO, Long id);
-
 	Boolean deleteCategory(Long id);
 }
